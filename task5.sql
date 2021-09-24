53602324aa18349f22427c4f1051daf98d5ec1f9
nik111215 <60958218+nik111215@users.noreply.github.com>
2021-09-24 19:54:47 +0300

    Update task5.sql

--- task5.sql
+++ task5.sql
@@ -36,34 +36,3 @@ from gas_station as gs
         on gs.id_gas_station = pl.id_gas_station) as g
     left join tank_farm tf
         on g.id_gas_station = tf.id_gas_station;
-
-
----CRUD запросы
-select *
-from staff;
-
-insert into staff(id_staff, id_gas_station, name, surname, position, salary) VALUES (197, 1, 'Константин', 'Маров', 'старший оператор АЗС', 57980 );
-
-update  staff
-    set salary = 55000
-    where  id_staff = 223;
-
-delete
-from staff
-where salary < 25000;
-
-
-
-
-select *
-from client;
-
-insert into client(id_client, name, surname, contact_email, number_vehicles) VALUES (10035, 'Прокофий', 'Аксёнов', 'prokak@rambler.com', 3);
-
-update client
-    set contact_email = 'andrtok@inbox.com'
-    where id_client = 10050;
-
-delete
-from client
-where number_vehicles > 5;
